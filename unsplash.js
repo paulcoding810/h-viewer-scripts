787c2a3b4ac64f699ae850aa9e19bcf07e25e128
paulcoding810 <41385034+longnghia@users.noreply.github.com>
2025-02-10 16:37:46 +0700

    update unsplash

--- unsplash.js
+++ unsplash.js
@@ -1,13 +1,22 @@
 function getPosts(url, page) {
-  var document = fetch(url);
-  var listElements = document.select(".jiBXB .GOfXh .oZXL2");
-  var posts = [];
+  const document = fetch(url).html();
+  const listElements = document.select(".Czj1u.eTw4k.r2uRM .J9bI_.Pc_c1.yZhvJ");
+  const fakeThumbnails = document.select(
+    "div div.AE2it div.I7e4t figure div.gFPnk div div.vthK5 div.gr86h a.mG0SP div.xH5KD img.tzC2N.fbGdz.cnmNG"
+  );
+  const posts = [];
+
+  let index = 0;
   listElements.forEach((element) => {
+    let thumb = fakeThumbnails ? fakeThumbnails.get(index) : null;
+    thumb = thumb ? thumb.attr("src") : null;
+
     posts.push({
       name: element.text(),
       url: element.absUrl("href"),
-      thumbnail: null,
+      thumbnail: thumb,
     });
+    index++;
   });
 
   return {
@@ -19,12 +28,12 @@ function getPosts(url, page) {
 
 function getImages(url, page) {
   const topic = url.split("https://unsplash.com/s/photos/")[1];
-  var json = xhr(
+  var json = fetch(
     "https://unsplash.com/napi/search/photos?page="
       .concat(page)
       .concat("&per_page=20&query=")
       .concat(topic)
-  );
+  ).json();
 
   var urls = json.results.map((image) => image.urls.regular);
 
